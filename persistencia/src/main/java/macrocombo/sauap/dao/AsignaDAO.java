326737cd6df3171d79e630b8f1d89319e9d23cd1
Konea <octavio.arellano@uabc.edu.mx>
2025-09-14 10:35:00 -0700

    YA ESTÁ TODA LA CONSULTA :D

--- persistencia/src/main/java/macrocombo/sauap/dao/AsignaDAO.java
+++ persistencia/src/main/java/macrocombo/sauap/dao/AsignaDAO.java
@@ -18,11 +18,13 @@ public class AsignaDAO extends AbstractDAO<Asigna> {
         return entityManager
                 .createQuery(
                         "SELECT a FROM Asigna a " +
-                                "LEFT JOIN FETCH a.idProfesor " +
-                                "LEFT JOIN FETCH a.idUnidad",
+                                "LEFT JOIN FETCH a.idProfesor p " +
+                                "LEFT JOIN FETCH a.idUnidad u " +
+                                "ORDER BY a.idProfesor.nombreProfesor ASC",
                         Asigna.class)
                 .getResultList();
     }
+
 
     @Override
     public EntityManager getEntityManager() {

c702e9fa9b9015c5b85300c0988dc23fb4ec39f8
marco <marcorico2002@gmail.com>
2025-09-14 20:26:50 -0700

    Realizar la barra de navegación: Ahora la barra de nevagacion tiene nuevo diseño, es modular, y mejor en todos los aspectos

--- persistencia/src/test/java/testDAO.java
+++ persistencia/src/test/java/testDAO.java
@@ -1,5 +1,7 @@
+import macrocombo.sauap.dao.AsignaDAO;
 import macrocombo.sauap.dao.ProfesorDAO;
 import macrocombo.sauap.dao.UnidadAprendizajeDAO;
+import macrocombo.sauap.entity.Asigna;
 import macrocombo.sauap.entity.UnidadAprendizaje;
 import macrocombo.sauap.persistence.HibernateUtil;
 import macrocombo.sauap.entity.Profesor;
@@ -9,14 +11,18 @@ import java.util.List;
 public class testDAO {
     public static void main(String[] args) {
         ProfesorDAO profesorDAO = new ProfesorDAO(HibernateUtil.getEntityManager());
-        UnidadAprendizajeDAO uaDAO = new UnidadAprendizajeDAO(HibernateUtil.getEntityManager());
-
+        UnidadAprendizajeDAO unidadAprendizajeDAO = new UnidadAprendizajeDAO(HibernateUtil.getEntityManager());
+        AsignaDAO asignaDAO = new AsignaDAO(HibernateUtil.getEntityManager());
         for (Profesor profe : profesorDAO.findAll()) {
             System.out.println(profe + "|| id [" + profe.getId()+ "]");
         }
 
-        for (UnidadAprendizaje ua : uaDAO.obtenerTodos()) {
+        for (UnidadAprendizaje ua : unidadAprendizajeDAO.obtenerTodos()) {
             System.out.println(ua + " || id [" + ua.getId() + "]");
+        }
+
+        for (Asigna asigna : asignaDAO.findAll()) {
+            System.out.println(asigna + "|| id [" + asigna.getId() + "]");
         }
     }
 }
